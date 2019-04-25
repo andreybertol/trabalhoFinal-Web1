00bb5c903459b64c0a7b8a7975a6d167e7230888
Andrey <andrey_hp@hotmail.com>
2019-04-24 22:24:28 -0300

    first commit

---
+++ src/main/java/br/edu/utfpr/pb/TrabalhoFinal/TrabalhoFinalApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.TrabalhoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalApplication.class, args);
+	}
+
+}
